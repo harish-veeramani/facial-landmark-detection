88619fd4b387d013c12978d719f0c9f5e339cbad
Harish Veeramani <hdv987@gmail.com>
2018-03-06 01:11:13 -0600

    Add image saving

--- app/src/main/java/com/harish/faciallandmarkdetection/MainActivity.java
+++ app/src/main/java/com/harish/faciallandmarkdetection/MainActivity.java
@@ -1,15 +1,27 @@
 package com.harish.faciallandmarkdetection;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.SurfaceView;
+import android.view.View;
 
 import org.opencv.android.BaseLoaderCallback;
 import org.opencv.android.CameraBridgeViewBase;
 import org.opencv.android.JavaCameraView;
 import org.opencv.android.OpenCVLoader;
+import org.opencv.android.Utils;
+import org.opencv.core.CvException;
+import org.opencv.core.CvType;
 import org.opencv.core.Mat;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity implements CameraBridgeViewBase.CvCameraViewListener2{
     JavaCameraView mJCV;
@@ -57,19 +69,70 @@ public class MainActivity extends AppCompatActivity implements CameraBridgeViewB
         if (mJCV != null) mJCV.disableView();
     }
 
+    private void saveImage(Mat subImg){
+        Bitmap bitmap = null;
+
+        try {
+            bitmap = Bitmap.createBitmap(subImg.cols(), subImg.rows(), Bitmap.Config.ARGB_8888);
+            Utils.matToBitmap(subImg, bitmap);
+        } catch (CvException e){
+            Log.d(TAG, e.getMessage());
+        }
+
+        subImg.release();
+
+        FileOutputStream out = null;
+        String filename = "frame.png";
+        File file = new File(Environment.getExternalStorageDirectory() + "/frames");
+
+        boolean success = true;
+        if (!file.exists()){
+            success = file.mkdir();
+        }
+
+        if (success){
+            File dest = new File(file, filename);
+
+            try {
+                out = new FileOutputStream(dest);
+                bitmap.compress(Bitmap.CompressFormat.PNG, 100, out);
+                //PNG is a lossless format so the compression factor is ignored
+            } catch (Exception e){
+                e.printStackTrace();
+                Log.d(TAG, e.getMessage());
+            } finally {
+                try {
+                    if (out != null){
+                        out.close();
+                        Log.d(TAG, "OK");
+                    }
+                } catch (IOException e){
+                    e.printStackTrace();
+                    Log.d(TAG, e.getMessage());
+                }
+            }
+        }
+    }
+
+    public void onClickGo(View view){
+        saveImage(mRGBA);
+        Intent intent = new Intent(this, DetailActivity.class);
+        startActivity(intent);
+    }
 
     @Override
     public void onCameraViewStarted(int width, int height) {
-
+        mRGBA = new Mat(height, width, CvType.CV_8UC4);
     }
 
     @Override
     public void onCameraViewStopped() {
-
+        mRGBA.release();
     }
 
     @Override
     public Mat onCameraFrame(CameraBridgeViewBase.CvCameraViewFrame inputFrame) {
-        return null;
+        mRGBA = inputFrame.rgba();
+        return mRGBA;
     }
 }
